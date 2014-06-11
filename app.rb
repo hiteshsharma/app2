90c0d4cee801d779fa034d5376ba20ccbfc237b6
Hitesh Sharma <hitesh@slideshare.com>
2014-06-11 19:28:54 +0530

    bbbff

--- app.rb
+++ app.rb
@@ -24,8 +24,8 @@ read_cookie = "(function(){
 set_cookie = "function setCookie(cname,cvalue,exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname+"="+cvalue+"; "+expires;
+    var expires = 'expires=' + d.toGMTString();
+    document.cookie = cname+'='+cvalue+'; '+expires;
 }
 "
 get '/testwrite' do
