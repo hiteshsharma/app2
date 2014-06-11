66238ed6b408d2f43fb313a072b52d5e28708f73
Hitesh Sharma <hitesh@slideshare.com>
2014-06-11 19:47:32 +0530

    aslkdfjafdffff

--- app.rb
+++ app.rb
@@ -3,25 +3,6 @@ set :protection, :except => :frame_options
 configure do
   mime_type :javascript, 'text/javascript'
 end
-read_cookie = "(function(){
-    var cookies;
-
-    function readCookie(name,c,C,i){
-        if(cookies){ return cookies[name]; }
-
-        c = document.cookie.split('; ');
-        cookies = {};
-
-        for(i=c.length-1; i>=0; i--){
-           C = c[i].split('=');
-           cookies[C[0]] = C[1];
-        }
-
-        return cookies[name];
-    }
-
-    window.readCookie = readCookie; // or expose it however you want
-})();"
 
 set_cookie = "function setCookie(cname,cvalue,exdays) {
     var d = new Date();
@@ -32,16 +13,10 @@ set_cookie = "function setCookie(cname,cvalue,exdays) {
 "
 get '/testwrite' do
   response.set_cookie "thirdparty", "set"
-  #redirect "/test-read"
   "<html>
 <head>
 <script type='text/javascript'>
-" + read_cookie +"
 window.location.replace('/test-read');
-function testCookie(){
-var p = document.getElementById('cookiestatus');
-p.innerText= window.readCookie('thirdparty') ? 'cookie set' : 'cookie not set';
-}
 </script>
 </head>
 <body onload=''>third party site loaded
